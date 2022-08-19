4ec758b5742daa70f06364964b3da3cc22498519
André Posso <admin@lzptec.com>
2022-08-18 21:12:59 -0300

    Adição de efeitos visuais.

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,23 @@
+import 'vanilla-tilt';
 import "./main.scss";
+
+const options: IntersectionObserverInit = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.8
+};
+
+const intersectionCallback: IntersectionObserverCallback = (entries) => entries
+    .forEach(entry => {
+        if (!entry.isIntersecting)
+            return;
+
+        entry.target.classList.add('animate');
+        observer.unobserve(entry.target);
+    });
+
+const observer = new IntersectionObserver(intersectionCallback, options);
+
+document
+    .querySelectorAll('.lazy')
+    .forEach(element => observer.observe(element));
